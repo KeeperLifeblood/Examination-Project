2dd3594f2161230e5103be70b3c84ef963cedd65
Keeper Lifeblood <Keeper.m7@gmail.com>
2024-12-27 00:32:01 -0400

    Modificacion de la interfaz de usuario Se cambió la paleta de colores de la interfaz de usuario se corrigió el error que no permitía ver los mensajes se corrigió el error que deformaba la app al cambiar el tamaño de la ventana se agregó un logo a la aplicación y se le cambió el nombre a AI Histroy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from "react";
 import "./App.css";
 import MessageList from "./components/MessageList";
+import logo from "./assets/logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>AI Messenger</h1>
+        <img src={logo} alt="logo" className="App-logo" />
+        <h1>
+          <span className="highlightA">A</span>
+          <span className="highlightI">I</span> History
+        </h1>
+        {/* <h3>Chat History</h3> */}
       </header>
       <div className="chat-container-wrapper">
         <MessageList />
